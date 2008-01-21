2574d7df52a939ea06fdf58fc122123ba184e696
cnwinds <cnwinds@b5e37a4f-7f3e-0410-a87c-11047d8cde73>
2008-01-21 08:08:50 +0000

    made a copy

---
+++ tags/2.0-beta2/MainFrm.pas
@@ -0,0 +1,299 @@
+unit MainFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient,
+  IdHTTP, ExtCtrls, MonitorBug, Menus, ImgList, ShellAPI, OleCtrls, SHDocVw,
+  mshtml, ActiveX, IeConst;
+
+type
+  TBugFreeHelperForm = class(TForm)
+    MainTrayIcon: TTrayIcon;
+    MainPopupMenu: TPopupMenu;
+    Timer1: TTimer;
+    ImageList1: TImageList;
+    TrayTimer: TTimer;
+    ImageList2: TImageList;
+    procedure TrayTimerTimer(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure MainPopupMenuPopup(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+    FMonitorBug: TMonitorBug;
+
+    procedure CreateMonitor;
+    procedure DestroyMonitor;
+
+    procedure BugCountChange(Sender: TObject; OldCount, NewCount: Integer);
+    procedure BugStatueChange(Sender: TObject; OldStatue, NewStatue: TMonitorBugStatue);
+  public
+    { Public declarations }
+    procedure MainClose(Sender: TObject);
+    procedure OpenBrowser(Sender: TObject);
+    procedure OpenSetting(Sender: TObject);
+    procedure ShowAboutForm(Sender: TObject);
+  end;
+
+var
+  BugFreeHelperForm: TBugFreeHelperForm;
+
+implementation
+
+uses SettingFrm, ConfigUnit, AboutFrm;
+
+{$R *.dfm}
+
+procedure TBugFreeHelperForm.BugCountChange(Sender: TObject; OldCount,
+  NewCount: Integer);
+begin
+  MainTrayIcon.BalloonTitle := '';
+
+  if NewCount = 0 then
+    MainTrayIcon.BalloonHint := Format('太好了，你已经清除了所有你的Bug :)', [])
+  else if OldCount < NewCount then
+    MainTrayIcon.BalloonHint := Format('注意：新发现了%d个Bug，现在你共有%d个Bug', [NewCount - OldCount, NewCount])
+  else
+    MainTrayIcon.BalloonHint := Format('恭喜了，减少了%d个Bug，现在你共有%d个Bug', [OldCount - NewCount, NewCount]);
+  MainTrayIcon.BalloonTimeout := 5000;
+  MainTrayIcon.BalloonFlags := bfInfo;
+  MainTrayIcon.ShowBalloonHint;
+end;
+
+procedure TBugFreeHelperForm.BugStatueChange(Sender: TObject; OldStatue,
+  NewStatue: TMonitorBugStatue);
+begin
+  case NewStatue of
+    mbsParamFail:
+    begin
+      MainTrayIcon.BalloonHint := Format('当前的参数无法登陆服务器，请先设置参数', []);
+      MainTrayIcon.BalloonTimeout := 5000;
+      MainTrayIcon.BalloonFlags := bfWarning;
+      MainTrayIcon.ShowBalloonHint;
+    end;
+    mbsLoginFail:
+    begin
+      MainTrayIcon.BalloonHint := Format('可能用户密码设置错误，请重新设置参数', []);
+      MainTrayIcon.BalloonTimeout := 5000;
+      MainTrayIcon.BalloonFlags := bfError;
+      MainTrayIcon.ShowBalloonHint;
+    end;
+    mbsHttpFail:
+    begin
+      MainTrayIcon.BalloonHint := Format('可能BugFree地址设置不正确或服务器暂时不能访问，请重新设置参数或让程序稍后重试', []);
+      MainTrayIcon.BalloonTimeout := 5000;
+      MainTrayIcon.BalloonFlags := bfWarning;
+      MainTrayIcon.ShowBalloonHint;
+    end;
+  end;
+end;
+
+procedure TBugFreeHelperForm.CreateMonitor;
+begin
+  if FMonitorBug <> nil then DestroyMonitor;
+
+  FMonitorBug := TMonitorBug.Create(Self);
+  FMonitorBug.BugFreeUrl := Config.BugFreeUrl;
+  FMonitorBug.UserName := Config.UserName;
+  FMonitorBug.UserPWD := Config.UserPWD;
+  FMonitorBug.UpdateIntervalSec := Config.UpdateIntervalSec;
+  FMonitorBug.BugChangeNotify := BugCountChange;
+  FMonitorBug.BugStatueChangeNotify := BugStatueChange;
+
+  FMonitorBug.Start;
+end;
+
+procedure TBugFreeHelperForm.DestroyMonitor;
+begin
+  if FMonitorBug <> nil then
+  begin
+    FMonitorBug.Stop;
+    FreeAndNil(FMonitorBug);
+  end;
+end;
+
+procedure TBugFreeHelperForm.FormCreate(Sender: TObject);
+begin
+  MainTrayIcon.Icons := ImageList1;
+  MainTrayIcon.Visible := True;
+  MainTrayIcon.IconIndex := 0;
+  MainTrayIcon.Hint := 'BugFree小助手';
+end;
+
+procedure TBugFreeHelperForm.FormDestroy(Sender: TObject);
+begin
+  DestroyMonitor;
+end;
+
+procedure TBugFreeHelperForm.MainClose(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TBugFreeHelperForm.OpenBrowser(Sender: TObject);
+begin
+  FMonitorBug.CloneNewWindow;
+end;
+
+procedure TBugFreeHelperForm.OpenSetting(Sender: TObject);
+begin
+  SettingForm.ShowModal;
+  DestroyMonitor;
+  CreateMonitor;
+end;
+
+procedure TBugFreeHelperForm.ShowAboutForm(Sender: TObject);
+begin
+  AboutForm.ShowModal;
+end;
+
+procedure TBugFreeHelperForm.Timer1Timer(Sender: TObject);
+begin
+  Timer1.Enabled := False;
+  CreateMonitor;
+end;
+
+procedure TBugFreeHelperForm.TrayTimerTimer(Sender: TObject);
+begin
+  if FMonitorBug = nil then
+  begin
+    MainTrayIcon.Icons := ImageList1;
+    MainTrayIcon.Animate := False;
+    MainTrayIcon.IconIndex := 0;
+    Exit;
+  end;
+
+  case FMonitorBug.State of
+    mbsInit:
+    begin
+      MainTrayIcon.Icons := ImageList1;
+      MainTrayIcon.Animate := False;
+      MainTrayIcon.IconIndex := 0;
+    end;
+    mbsWaitLoginPage, mbsWaitLoginResult:
+    begin
+      MainTrayIcon.Icons := ImageList1;
+      MainTrayIcon.Animate := False;
+      MainTrayIcon.IconIndex := (MainTrayIcon.IconIndex + 1) mod 2
+    end;
+    mbsIdel, mbsWaitQueryPage, mbsWaitQueryResult:
+    begin
+      if FMonitorBug.BugCount = 0 then
+      begin
+        MainTrayIcon.Icons := ImageList1;
+        MainTrayIcon.Animate := False;
+        MainTrayIcon.IconIndex := 1;
+      end else begin
+        MainTrayIcon.Icons := ImageList2;
+        MainTrayIcon.AnimateInterval := 200;
+        MainTrayIcon.Animate := True;
+      end;
+    end;
+    mbsParamFail, mbsLoginFail, mbsHttpFail:
+    begin
+      MainTrayIcon.Icons := ImageList1;
+      MainTrayIcon.Animate := False;
+      MainTrayIcon.IconIndex := 2;
+    end;
+  end;
+end;
+
+procedure TBugFreeHelperForm.MainPopupMenuPopup(Sender: TObject);
+const
+  MenuTipInfo: array[0..11] of string = (
+    '准备登陆',
+    '正在等待登录页面返回...)',
+    '正在验证登陆，耐心等一下 :)',
+    '恭喜你[%s]，没有你的Bug',
+    '[%s]未解决Bug%d个...',
+    '正在获取最新的bug数...',
+    '请设置必要的参数...',
+    '[%s]密码设错误，赶紧重新设置...',
+    '访问服务器失败',
+
+    '版本[%d.%d]beta2...',
+    '设置参数...',
+    '退出');
+var
+  Ver: Cardinal;
+
+  function CreateMenuItem(Caption: string; Event: TNotifyEvent): TMenuItem;
+  begin
+    Result := TMenuItem.Create(nil);
+    Result.Caption := Caption;
+    Result.OnClick := Event;
+  end;
+
+  procedure AddMenuItem(Caption: string; Event: TNotifyEvent);
+  begin
+    MainPopupMenu.Items.Add(CreateMenuItem(Caption, Event));
+  end;
+
+begin
+  MainPopupMenu.Items.Clear;
+
+  Ver := GetFileVersion(Application.ExeName);
+
+  AddMenuItem(Format(MenuTipInfo[9], [Ver shr 16, Ver and $ffff]),
+    ShowAboutForm);
+  AddMenuItem(Format('-', []), nil);
+
+  if FMonitorBug <> nil then
+  begin
+    case FMonitorBug.State of
+      mbsInit:
+      begin
+        AddMenuItem(Format(MenuTipInfo[0], []), nil);
+      end;
+      mbsWaitLoginPage:
+      begin
+        AddMenuItem(Format(MenuTipInfo[1], []), nil);
+      end;
+      mbsWaitLoginResult:
+      begin
+        AddMenuItem(Format(MenuTipInfo[2], []), nil);
+      end;
+      mbsIdel:
+      begin
+        if FMonitorBug.BugCount = 0 then
+          AddMenuItem(Format(MenuTipInfo[3],
+            [FMonitorBug.UserName, FMonitorBug.BugCount]), OpenBrowser)
+        else
+          AddMenuItem(Format(MenuTipInfo[4],
+            [FMonitorBug.UserName, FMonitorBug.BugCount]), OpenBrowser);
+      end;
+      mbsWaitQueryPage, mbsWaitQueryResult:
+      begin
+        if FMonitorBug.BugCount = 0 then
+          AddMenuItem(Format(MenuTipInfo[5],
+            [FMonitorBug.UserName, FMonitorBug.BugCount]), OpenBrowser)
+        else
+          AddMenuItem(Format(MenuTipInfo[4],
+            [FMonitorBug.UserName, FMonitorBug.BugCount]), OpenBrowser);
+      end;
+      mbsParamFail:
+      begin
+        AddMenuItem(Format(MenuTipInfo[6], []), OpenSetting);
+      end;
+      mbsLoginFail:
+      begin
+        AddMenuItem(Format(MenuTipInfo[7],
+          [FMonitorBug.UserName]), OpenSetting)
+      end;
+      mbsHttpFail:
+      begin
+        AddMenuItem(Format(MenuTipInfo[8], []), OpenSetting);
+      end;
+    end;
+  end;
+
+  AddMenuItem('-', nil);
+  AddMenuItem(MenuTipInfo[10], OpenSetting);
+  AddMenuItem('-', nil);
+  AddMenuItem(MenuTipInfo[11], MainClose);
+end;
+
+end.
---
+++ tags/2.0-beta2/SettingFrm.dfm
@@ -0,0 +1,125 @@
+object SettingForm: TSettingForm
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  Caption = #21442#25968#37197#32622
+  ClientHeight = 226
+  ClientWidth = 309
+  Color = clBtnFace
+  Font.Charset = GB2312_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = #23435#20307
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poDesktopCenter
+  Scaled = False
+  OnShow = FormShow
+  DesignSize = (
+    309
+    226)
+  PixelsPerInch = 96
+  TextHeight = 12
+  object Label1: TLabel
+    Left = 24
+    Top = 24
+    Width = 78
+    Height = 12
+    Caption = 'BugFree'#20027#39029#65306
+  end
+  object Label2: TLabel
+    Left = 52
+    Top = 64
+    Width = 48
+    Height = 12
+    Caption = #29992#25143#21517#65306
+  end
+  object Label3: TLabel
+    Left = 64
+    Top = 96
+    Width = 36
+    Height = 12
+    Caption = #23494#30721#65306
+  end
+  object Label4: TLabel
+    Left = 16
+    Top = 134
+    Width = 84
+    Height = 12
+    Caption = #26356#26032#38388#38548#26102#38388#65306
+  end
+  object Label5: TLabel
+    Left = 277
+    Top = 134
+    Width = 12
+    Height = 12
+    Caption = #31186
+  end
+  object Edit1: TEdit
+    Left = 106
+    Top = 21
+    Width = 183
+    Height = 20
+    TabOrder = 0
+    OnChange = Edit1Change
+  end
+  object Edit2: TEdit
+    Left = 106
+    Top = 61
+    Width = 183
+    Height = 20
+    TabOrder = 1
+    OnChange = Edit1Change
+  end
+  object Edit3: TEdit
+    Left = 106
+    Top = 93
+    Width = 183
+    Height = 20
+    PasswordChar = '*'
+    TabOrder = 2
+    OnChange = Edit1Change
+  end
+  object Button1: TButton
+    Left = 118
+    Top = 197
+    Width = 75
+    Height = 21
+    Anchors = [akRight, akBottom]
+    Caption = #20445#23384
+    Default = True
+    TabOrder = 3
+    OnClick = Button1Click
+    ExplicitTop = 174
+  end
+  object Button2: TButton
+    Left = 216
+    Top = 197
+    Width = 75
+    Height = 21
+    Anchors = [akRight, akBottom]
+    Caption = #20851#38381
+    TabOrder = 4
+    OnClick = Button2Click
+    ExplicitTop = 174
+  end
+  object SpinEdit1: TSpinEdit
+    Left = 106
+    Top = 131
+    Width = 165
+    Height = 21
+    MaxValue = 0
+    MinValue = 0
+    TabOrder = 5
+    Value = 0
+    OnChange = Edit1Change
+  end
+  object CheckBox1: TCheckBox
+    Left = 16
+    Top = 168
+    Width = 111
+    Height = 17
+    Caption = #38543#30005#33041#33258#21160#21551#21160
+    TabOrder = 6
+  end
+end
